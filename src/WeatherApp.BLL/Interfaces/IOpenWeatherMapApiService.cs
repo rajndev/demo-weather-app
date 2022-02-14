d1e4064e5fe6394260b9543ac63a5aa70b0e39dc
rajndev <rajnsoftwaredev@gmail.com>
2022-02-14 11:45:33 -0500

    Implement the Refit ApiClient and in place of HttpClient

---
+++ src/WeatherApp.BLL/Interfaces/IOpenWeatherMapApiService.cs
@@ -0,0 +1,15 @@
+﻿using Refit;
+using System.Threading.Tasks;
+using WeatherApp.BLL.Models;
+
+namespace WeatherApp.BLL.Interfaces
+{
+    public interface IOpenWeatherAppApiService
+    {
+        [Get("/weather?id={cityCode}&appid={apiKey}&units=imperial")]
+        public Task<ApiResponse<WeatherInfoRoot>> GetWeatherInfoByCityCode(int? cityCode, string apiKey);
+
+        [Get("/weather?q={cityName}&appid={apiKey}&units=imperial")]
+        public Task<ApiResponse<WeatherInfoRoot>> GetWeatherInfoByCityName(string cityName, string apiKey);
+    }
+}
