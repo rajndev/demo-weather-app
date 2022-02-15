68d55b81b6e76e764c2fa6ad247d01b746a26af4
rajndev <rajnsoftwaredev@gmail.com>
2022-02-14 20:08:03 -0500

    Add dependencies extension methods for each layer

---
+++ src/WeatherApp.Web/DependencyInjection/AddControllerServiceCollections.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WeatherApp.HelperClasses;
+
+namespace WeatherApp.Web.DependencyInjection
+{
+    public static class AddControllerServiceCollections
+    {
+        public static IServiceCollection AddControllerDependencies(this IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+            services.AddAutoMapper(c => c.AddProfile<AutoMappingProfile>(), typeof(Startup));
+            return services;
+        }
+    }
+}
