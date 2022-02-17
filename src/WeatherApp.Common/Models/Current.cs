de186debe6897757dfd3b11062268bb00f25c329
rajndev <rajnsoftwaredev@gmail.com>
2022-02-17 14:13:44 -0500

    Refactor the OpenWeatherMapApi implementation to call an alternative endpoint and clean up the project

---
+++ src/WeatherApp.Common/Models/Current.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherApp.Common.Models
+{
+    public class Current
+    {
+        public double Temp { get; set; }
+        public List<Weather> Weather { get; set; }
+        public long Sunrise { get; set; }
+        public long Sunset { get; set; }
+        public long Dt { get; set; }
+    }
+}
--- src/WeatherApp.Provider/WeatherServiceProvider.cs
+++ src/WeatherApp.Provider/WeatherServiceProvider.cs
@@ -1,13 +1,12 @@
 ﻿using AutoMapper;
 using GoogleMaps.LocationServices;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
+using NodaTime;
 using System.Globalization;
 using WeatherApp.ApiClient.Interfaces;
 using WeatherApp.Common.Models;
 using WeatherApp.Data.Provider.DataContext;
-using WeatherApp.Data.Provider.Entities;
 using WeatherApp.Provider.Interfaces;
 
 namespace WeatherApp.Provider
@@ -40,32 +39,26 @@ namespace WeatherApp.Provider
 
         public async Task<ProviderResult<WeatherData>> GetCurrentWeather(string cityName)
         {
-            var fdsa = GetCityCoordinates(cityName);
+            var cityCoordinates = GetCityCoordinates(cityName);
+            var lat = cityCoordinates?.Item1;
+            var lon = cityCoordinates?.Item2;
 
-            var split = cityName.Split(",");
-
-            int? cityCode = await GetCityCodeAsync(split, split.Length);
-
-            var apiResponse = cityCode != null ?
-                await _apiService.GetWeatherInfoByCityCode(cityCode, _options.OpenWeatherMapApiKey) :
-                await _apiService.GetWeatherInfoByCityName(cityName, _options.OpenWeatherMapApiKey);
+            var apiResponse = await _apiService.GetWeatherInfo(lat, lon, _options.OpenWeatherMapApiKey);
 
             var providerResult = _mapper.Map<ProviderResult<WeatherData>>(apiResponse);
 
             if ((int)apiResponse.StatusCode == 200)
             {
-                var currentDateTime = GetDateTimeFromEpoch(
-                        apiResponse.Content.Sys.Sunrise,
-                        apiResponse.Content.Sys.Sunset,
-                        apiResponse.Content.Dt,
-                        apiResponse.Content.Timezone
-                    );
+                var zoneDateTime = GetZoneDateTime(
+                    apiResponse.Content.TimeZone,
+                    apiResponse.Content.Current.Dt,
+                    apiResponse.Content.Current.Sunrise,
+                    apiResponse.Content.Current.Sunset);
 
-                providerResult.Content.CityDate = currentDateTime.Item1;
-                providerResult.Content.CityTime = currentDateTime.Item2;
-                providerResult.Content.IsDayTime = currentDateTime.Item3;
+                providerResult.Content.CityDate = zoneDateTime.Item1;
+                providerResult.Content.CityTime = zoneDateTime.Item2;
+                providerResult.Content.IsDayTime = zoneDateTime.Item3;
 
-                //capitalize each word in the city name
                 cityName = CapitalizeCityName(cityName);
                 providerResult.Content.CityName = cityName;
             }
@@ -75,15 +68,16 @@ namespace WeatherApp.Provider
 
         public Tuple<string, string> GetCityCoordinates(string cityName)
         {
-            // var address = cityName;
-
             var locationService = new GoogleLocationService(_options.GoogleApiKey);
-            var point = locationService.GetLatLongFromAddress("Pittsburgh, PA");
+            var point = locationService.GetLatLongFromAddress(cityName);
 
-            var latitude = point.Latitude.ToString();
-            var longitude = point.Longitude.ToString();
-
-            return Tuple.Create(latitude, longitude);
+            if (point != null)
+            {
+                var lat = point.Latitude.ToString();
+                var lon = point.Longitude.ToString();
+                return Tuple.Create(lat, lon);
+            }
+            return null;
         }
 
         private string CapitalizeCityName(string cityName)
@@ -93,32 +87,21 @@ namespace WeatherApp.Provider
             return cityName;
         }
 
-        private Tuple<string, string, bool> GetDateTimeFromEpoch(long sunrise, long sunset, long currentTime, long timezone)
+        private Tuple<string, string, bool> GetZoneDateTime(string timeZone, long currentTime, long sunrise, long sunset)
         {
-            DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(currentTime + timezone);
+            var now = SystemClock.Instance.GetCurrentInstant();
+
+            var dtzi = DateTimeZoneProviders.Tzdb;
+            var timeZoneToken = dtzi[timeZone];
+
+            var seattleNow = new ZonedDateTime(now, timeZoneToken);
+
+            var displayDate = seattleNow.LocalDateTime.ToDateTimeUnspecified().ToString("D");
+            var displayTime = seattleNow.LocalDateTime.ToDateTimeUnspecified().ToShortTimeString();
 
             bool isDaytime = currentTime > sunrise && currentTime < sunset;
 
-            var displayDate = dateTimeOffset.DateTime.ToString("D");
-            var displayTime = dateTimeOffset.DateTime.ToString("t");
-
             return Tuple.Create(displayDate, displayTime, isDaytime);
-        }
-
-        public async Task<int?> GetCityCodeAsync(string[] split, int length)
-        {
-            City cityRecord = null;
-
-            if (length == 3)
-            {
-                cityRecord = await _context.Cities.Where(p => p.Name == split[0].Trim() && p.State == split[1].Trim() && p.Country == split[2].Trim()).FirstOrDefaultAsync();
-            }
-            else if (length == 2)
-            {
-                cityRecord = await _context.Cities.Where(p => p.Name == split[0].Trim() && p.State == split[1].Trim()).FirstOrDefaultAsync();
-            }
-
-            return cityRecord?.CityCode;
         }
     }
 }
